348a978f45a939710f09610b4f5a96eadadee6a3
David Freitas <david.freitas@voxline.com.br>
2023-04-17 15:47:23 -0300

    atualizacao navbar e portfolio

--- js/main.js
+++ js/main.js
@@ -1,25 +1,15 @@
 /** 
  * Menu Toggle
  */
+const nav = document.querySelector('.nav')
 const menuLink = document.querySelector('.nav__list')
 const menuToggle = document.querySelector('.menu__toggle')
 
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault()
 
-    const menuIcon = document.querySelector('.menu__toggle i')
-    const classIcon = menuIcon.classList.item(1)
-
-    if (classIcon === 'fa-bars') {
-        menuIcon.classList.remove('fa-bars')
-        menuIcon.classList.add('fa-times')
-    } else {
-        menuIcon.classList.remove('fa-times')
-        menuIcon.classList.add('fa-bars')
-    }
-
-    const nav = document.querySelector('.nav')
     nav.classList.toggle('active')
+    menuToggle.classList.toggle('active')
 
     const active = nav.classList.contains('active')
     event.currentTarget.setAttribute('aria-expanded', active)
