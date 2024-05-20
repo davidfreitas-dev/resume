da0af2fff33a8204a9181a1af5b03fa06ea0c1c7
David Freitas <davidfreitas.dev@outlook.com>
2024-05-20 18:11:46 -0300

    add classe de link ativo de acordo com a rolagem da pagina e refatoracao do codigo de aplicacao do thema de cores

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,42 @@
-$('.nav a[href^="#"]').on('click', function(e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-    targetOffset = $(id).offset().top;
+$(document).ready(function() {
+    // Função para navegação suave
+    function smoothScroll() {
+        $('.nav a[href^="#"]').on('click', function(e) {
+            e.preventDefault();
+            const targetId = $(this).attr('href');
+            const targetOffset = $(targetId).offset().top;
+            
+            $('html, body').animate({ 
+                scrollTop: targetOffset - 50
+            }, 500);
+        });
+    }
+
+    // Função para atualizar a classe ativa nos links de navegação
+    function updateActiveNavLink() {
+        const scrollPos = $(document).scrollTop();
+
+        $('.nav__link').each(function() {
+            const currLink = $(this);
+            const refElement = $(currLink.attr('href'));
+            const elementTop = refElement.position().top;
+            const elementHeight = refElement.height();
+
+            if (elementTop <= scrollPos + 50 && elementTop + elementHeight > scrollPos + 50) {
+                $('.nav__link').removeClass('active');
+                currLink.addClass('active');
+            } else {
+                currLink.removeClass('active');
+            }
+        });
+    }
+
+    // Inicializa a navegação suave
+    smoothScroll();
+
+    // Atualiza a classe ativa nos links de navegação ao rolar a página
+    $(window).on('scroll', updateActiveNavLink);
     
-    $('html, body').animate({ 
-        scrollTop: targetOffset - 50
-    }, 500);
+    // Atualiza a classe ativa ao carregar a página
+    updateActiveNavLink();
 });
--- js/theme.js
+++ js/theme.js
@@ -1,52 +1,52 @@
 $(function() {
-  const currentTheme = localStorage.getItem('theme');
-  
-  if (!currentTheme) {
-      applySystemColorScheme();
-  } else {
-      $('html').attr('data-theme', currentTheme);
-      changeBtnIcon(currentTheme);
-  }
-  
-  function applySystemColorScheme() {
-      const colorScheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-
-      if (colorScheme === 'dark') {
-          $('html').attr('data-theme', 'dark');
-      } else {
-          $('html').attr('data-theme', 'light');
-      }
-
-      changeBtnIcon(colorScheme);
-  }
-
-  function changeBtnIcon(theme) {
-      const btnIcon = $('.btn__toggle__theme span');
-      
-      if (theme === 'dark') {
-          btnIcon.removeClass('la-moon').addClass('la-sun');
-      } else {
-          btnIcon.removeClass('la-sun').addClass('la-moon');
-      }
-  }
-
+  const themeKey = 'theme';
   const themeToggleBtn = $('.btn__toggle__theme');
 
-  function toggleTheme() {
-      const currentTheme = localStorage.getItem('theme');
+  // Inicialização do tema
+  initializeTheme();
 
-      if (currentTheme === 'light') {
-          $('html').attr('data-theme', 'dark');
-          localStorage.setItem('theme', 'dark');
-          changeBtnIcon('dark');
-      } else {
-          $('html').attr('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-          changeBtnIcon('light');
-      }      
+  // Inicialização do tema com base no armazenamento local ou esquema de cores do sistema
+  function initializeTheme() {
+    const storedTheme = localStorage.getItem(themeKey);
+
+    if (storedTheme) {
+      applyTheme(storedTheme);
+    } else {
+      applySystemColorScheme();
+    }
   }
 
+  // Aplica o esquema de cores do sistema
+  function applySystemColorScheme() {
+    const prefersDarkScheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const systemTheme = prefersDarkScheme ? 'dark' : 'light';
+    applyTheme(systemTheme);
+  }
+
+  // Aplica o tema e atualiza o ícone do botão
+  function applyTheme(theme) {
+    $('html').attr('data-theme', theme);
+    updateButtonIcon(theme);
+    localStorage.setItem(themeKey, theme);
+  }
+
+  // Atualiza o ícone do botão com base no tema
+  function updateButtonIcon(theme) {
+    const btnIcon = $('.btn__toggle__theme span');
+    btnIcon.toggleClass('la-moon', theme === 'light');
+    btnIcon.toggleClass('la-sun', theme === 'dark');
+  }
+
+  // Alterna entre os temas claro e escuro
+  function toggleTheme() {
+    const currentTheme = $('html').attr('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    applyTheme(newTheme);
+  }
+
+  // Evento de clique no botão de alternância de tema
   themeToggleBtn.on('click', toggleTheme);
-  
+
+  // Descomente a linha abaixo se desejar aplicar o tema automaticamente quando o esquema de cores do sistema mudar
   // window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', applySystemColorScheme);
 });
